f53250f8269233439e16f80804a2f63a19c1f0bd
Diego Lucas <lukasxdp2@gmail.com>
2021-07-08 18:53:02 -0300

    Create method verification of session

--- App/Controllers/AppController.php
+++ App/Controllers/AppController.php
@@ -6,14 +6,32 @@
 
     public function handleTimeline(){
       session_start();
-      $id = $_SESSION['id'];
-      $name = $_SESSION['name'];
-
-      if($name != '' && $id != ''){
+      $isAuthenticated = $this->verifySession('id', 'name');
+      if($isAuthenticated){
+        $tweet = Container::getModel('Tweet');
+        $tweets = $tweet->getTweets($_SESSION['id']);
+        $this->view->tweets = $tweets;
         $this->render('timeline');
       }else{
         header('Location: /?login=error');
       }
     }
+
+    public function tweet(){
+      session_start();
+      $isAuthenticated = $this->verifySession('id', 'name');
+      if($isAuthenticated){
+        $tweet = Container::getModel('Tweet');
+        $tweet->__set('tweet', $_POST['tweet']);
+        $tweet->__set('user_id', $_SESSION['id']);
+        $tweet->saveTweet();
+        header('Location: /timeline');
+      }else{
+        header('Location: /?login=error');
+      }
+      
+    }
+
+    
   }
 ?>
