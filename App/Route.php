a38192abf55cb5e6616001321b645caf1bb0e593
Diego Lucas <lukasxdp2@gmail.com>
2021-07-08 18:51:27 -0300

    Add route tweet

--- App/Route.php
+++ App/Route.php
@@ -40,6 +40,12 @@ class Route extends Bootstrap{
         'action' => 'logout'
       );
 
+      $routes['tweet'] = array(
+        'route' => '/tweet',
+        'controller' => 'AppController',
+        'action' => 'tweet'
+      );
+
       
       $this->setRoutes($routes);
     }
